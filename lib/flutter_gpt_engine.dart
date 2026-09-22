f678de931d490da9456d880b89c5f58cdd6cf12a
Nafim Ahmed <recentnafimahmed@gmail.com>
2026-09-22 15:20:20 +0600

    feat: export performance tuning API

--- lib/flutter_gpt_engine.dart
+++ lib/flutter_gpt_engine.dart
@@ -6,6 +6,7 @@ export 'src/local_llm_config.dart';
 export 'src/local_llm_generation_event.dart';
 export 'src/local_llm_message.dart';
 export 'src/local_llm_model.dart';
+export 'src/local_llm_performance.dart';
 export 'src/web_search_config.dart';
 export 'src/web_search_models.dart';
 export 'src/web_search_service.dart';
